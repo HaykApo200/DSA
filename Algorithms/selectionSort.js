ff4d37ce637310e61e18b018d4d1d66ceab67ffd
HaykApo200 <haykapoyan0103@gmail.com>
2025-04-26 16:27:18 +0400

    addDescription

--- Algorithms/selectionSort.js
+++ Algorithms/selectionSort.js
@@ -1,3 +1,10 @@
+//SelectionSort // Time Complexity: O(n^2) // Space Complexity: O(1)
+
+// How it works:
+// 1.Go through the array to find the lowest value.
+// 2.Move the lowest value to the front of the unsorted part of the array.
+// 3.Go through the array again as many times as there are values in the array.
+
 function selectionSort(arr){
     let index;
     let change = false;
